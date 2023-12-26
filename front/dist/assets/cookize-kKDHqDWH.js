9c2c16874b1d4405afb4171794f62e90159104f4
Kazerlelutin <b@bouteiller.contact>
2023-12-26 03:05:27 +0100

    refresh build

---
+++ front/dist/assets/cookize-kKDHqDWH.js
@@ -0,0 +1,8 @@
+const e=`<template id="cookize">\r
+  <div class="text-[6rem] leading-[5.5rem] font-InterBold m-0" _="on load getCookize end">\r
+    <div>\r
+      <h1 class="text-[6rem] leading-[5.5rem] font-InterBold m-0"><span data-type="c">C</span><span data-type="counter"\r
+          class="transition-all duration-200">oo</span><span data-type="kiz">kize</span></h1>\r
+    </div>\r
+  </div>\r
+</template>`;export{e as default};
