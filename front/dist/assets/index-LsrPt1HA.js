d74f2f98969f1d687350a37715bba43c3a77be15
Kazerlelutin <b@bouteiller.contact>
2023-12-26 03:15:25 +0100

    change page

---
+++ front/dist/assets/index-LsrPt1HA.js
@@ -0,0 +1,27 @@
+const t=`<div class="grid grid-rows-[auto_1fr_auto] h-[100dvh]">\r
+  <br _="on load template 'header' end" />\r
+\r
+  <div class="grid grid-cols-2 gap-2 justify-center items-center p-4">\r
+\r
+    <div class="ml-6">\r
+      <br _="on load template 'cookize' end" data-bind="c" />\r
+      <div class="ml-7 italic" id="multiplier" data-trans="tagline">Enchanced your Team</div>\r
+      <div class="mt-5 flex gap-3">\r
+        <button _="on click secret 'get-started' end" data-trans="get-started"\r
+          class="rounded-full border-white border bg-white text-black px-3 py-2 transition-all duration-100 hover:bg-black hover:text-white">\r
+          Get started\r
+        </button>\r
+        <button _="on click secret 'doc' end" data-trans="documentation"\r
+          class="rounded-full border-white border px-3 py-2 transition-all duration-100 hover:bg-white hover:text-black ">\r
+          Documentation\r
+        </button>\r
+\r
+\r
+      </div>\r
+    </div>\r
+    <br _="on load template 'cookie' end" />\r
+  </div>\r
+  <br _="on load template 'footer' end" />\r
+\r
+  <span class="fixed" _="on load translate end"></span>\r
+</div>`;export{t as default};
--- front/main.js
+++ front/main.js
@@ -1,12 +1,10 @@
 import "./statics/style.css"
 import "./statics/_hyperscript.min.js"
-import index from "./pages/index.html?raw"
-import home from "./pages/home.html?raw"
 
 import { kll } from "../libs/kll.js"
 ;(async () => {
   kll({
-    "/": index,
-    "/home": home,
+    "/": await import("./pages/index.html?raw"),
+    "/home": await import("./pages/index.html?raw"),
   })
 })()
